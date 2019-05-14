ad49f8084e13389e95f54545f34b1fb22623618a
brenj <bwhite@nowsecure.com>
2019-05-14 15:21:10 -0400

    Update migrations to include updated contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,11 @@
-// migrating the appropriate contracts
-var FarmerRole = artifacts.require("./FarmerRole.sol");
+var CompanyRole = artifacts.require("./CompanyRole.sol");
 var DistributorRole = artifacts.require("./DistributorRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(FarmerRole);
+  deployer.deploy(CompanyRole);
   deployer.deploy(DistributorRole);
   deployer.deploy(RetailerRole);
   deployer.deploy(ConsumerRole);
