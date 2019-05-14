21adf5934b60d955a0569950cf0fb193a6d3c106
brenj <bwhite@nowsecure.com>
2019-05-14 15:20:39 -0400

    Add test cases for CompanyRole

---
+++ test/TestCompanyRole.js
@@ -0,0 +1,43 @@
+const CompanyRole = artifacts.require('CompanyRole');
+const REVERT_ERROR_MSG = 'VM Exception while processing transaction: revert';
+
+contract('CompanyRole', function(accounts) {
+  const ownerID = accounts[0];
+  const companyID = accounts[1];
+  const retailerID = accounts[2];
+
+  it("Testing CompanyRole", async() => {
+    const companyRole = await CompanyRole.deployed();
+
+    let eventEmitted = false;
+    let event = companyRole.CompanyAdded();
+    event.watch((err, res) => {
+      eventEmitted = true;
+    });
+
+    let errorThrown;
+
+    try {
+      await companyRole.addCompany(companyID, { from: retailerID });
+    } catch (error) {
+      errorThrown = error;
+    }
+    assert.notEqual(
+      errorThrown, undefined, 'Revert error not thrown for onlyCompany');
+    assert.isAbove(
+      errorThrown.message.search(REVERT_ERROR_MSG),
+      -1, `Revert error not thrown for onlyCompany`);
+
+    assert.equal(
+      await companyRole.isCompany(
+        companyID), false, 'Shouldn\'t be a valid company');
+    await companyRole.addCompany(companyID, { from: ownerID });
+    assert.equal(
+      await companyRole.isCompany(
+        companyID), true, 'Should be a valid company');
+
+    // TODO: Why doesn't this pass even though an event is emitted?
+    assert.equal(eventEmitted, true, 'Should have emitted an event');
+  });
+});
+
