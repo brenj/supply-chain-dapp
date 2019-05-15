e55336e2a70a041f30deee9222484b749097ce65
brenj <bwhite@nowsecure.com>
2019-05-14 20:35:19 -0400

    Add ManufacturerRole

--- contracts/access_control/ManufacturerRole.sol
+++ contracts/access_control/ManufacturerRole.sol
@@ -1,54 +1,44 @@
 pragma solidity ^0.4.24;
 
-// Import the library 'Roles'
 import "./Roles.sol";
 
-// Define a contract 'FarmerRole' to manage this role - add, remove, check
-contract FarmerRole {
-  using Roles for Roles.Role;
+contract ManufacturerRole {
+    using Roles for Roles.Role;
 
-  // Define 2 events, one for Adding, and other for Removing
-  event FarmerAdded(address indexed account);
-  event FarmerRemoved(address indexed account);
+    event ManufacturerAdded(address newManufacturer);
+    event ManufacturerRemoved(address manufacturer);
 
-  // Define a struct 'farmers' by inheriting from 'Roles' library, struct Role
-  Roles.Role private farmers;
+    Roles.Role private manufacturers;
 
-  // In the constructor make the address that deploys this contract the 1st farmer
-  constructor() public {
-    _addFarmer(msg.sender);
-  }
+    constructor() public {
+        _addManufacturer(msg.sender);
+    }
 
-  // Define a modifier that checks to see if msg.sender has the appropriate role
-  modifier onlyFarmer() {
-    require(isFarmer(msg.sender));
-    _;
-  }
+    modifier onlyManufacturer() {
+        require(
+            isManufacturer(msg.sender), 'Only a manufacturer can do this');
+        _;
+    }
 
-  // Define a function 'isFarmer' to check this role
-  function isFarmer(address account) public view returns (bool) {
-    return farmers.has(account);
-  }
+    function isManufacturer(address account) public view returns (bool) {
+        return manufacturers.has(account);
+    }
 
-  // Define a function 'addFarmer' that adds this role
-  function addFarmer(address account) public onlyFarmer {
-    _addFarmer(account);
-  }
+    function addManufacturer(address account) public onlyManufacturer {
+        _addManufacturer(account);
+    }
 
-  // Define a function 'renounceFarmer' to renounce this role
-  function renounceFarmer() public {
-    _removeFarmer(msg.sender);
-  }
+    function renounceManufacturer() public {
+        _removeManufacturer(msg.sender);
+    }
 
-  // Define an internal function '_addFarmer' to add this role, called by 'addFarmer'
-  function _addFarmer(address account) internal {
-    farmers.add(account);
-    emit FarmerAdded(account);
-  }
+    function _addManufacturer(address account) internal {
+        manufacturers.add(account);
+        emit ManufacturerAdded(account);
+    }
 
-  // Define an internal function '_removeFarmer' to remove this role, called by 'removeFarmer'
-  function _removeFarmer(address account) internal {
-    farmers.remove(account);
-    emit FarmerRemoved(account);
-  }
+    function _removeManufacturer(address account) internal {
+        manufacturers.remove(account);
+        emit ManufacturerRemoved(account);
+    }
 }
