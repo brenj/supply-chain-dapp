18078a2b708467730930eb040eaf76617892f513
brenj <bwhite@nowsecure.com>
2019-05-18 11:30:06 -0400

    Uncomment provider code

--- web/js/app.js
+++ web/js/app.js
@@ -57,26 +57,25 @@ App = {
     initWeb3: async function () {
         /// Find or Inject Web3 Provider
         /// Modern dapp browsers...
-        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-        // if (window.ethereum) {
-        //     App.web3Provider = window.ethereum;
-        //     try {
-        //         // Request account access
-        //         await window.ethereum.enable();
-        //     } catch (error) {
-        //         // User denied account access...
-        //         console.error("User denied account access")
-        //     }
-        // }
-        // // Legacy dapp browsers...
-        // else if (window.web3) {
-        //     App.web3Provider = window.web3.currentProvider;
-        // }
-        // // If no injected web3 instance is detected, fall back to Ganache
-        // else {
-        //     console.log("GOT HERE");
-        //     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-        // }
+        if (window.ethereum) {
+            App.web3Provider = window.ethereum;
+            try {
+                // Request account access
+                await window.ethereum.enable();
+            } catch (error) {
+                // User denied account access...
+                console.error("User denied account access")
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = window.web3.currentProvider;
+        }
+        // If no injected web3 instance is detected, fall back to Ganache
+        else {
+            console.log("GOT HERE");
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        }
 
         App.getMetaskAccountID();
 
