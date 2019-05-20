3fdf9e990b7a705cf73c06215196d19f978f1d53
brenj <bwhite@nowsecure.com>
2019-05-19 22:56:48 -0400

    Add role contracts back
     Tests only work if these are present even though they are
     required for deployment.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 var SupplyChain = artifacts.require("./SupplyChain.sol");
+var CompanyRole = artifacts.require("./CompanyRole.sol");
+var ManufacturerRole = artifacts.require("./ManufacturerRole.sol");
+var RetailerRole = artifacts.require("./RetailerRole.sol");
+var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SupplyChain);
+  deployer.deploy(CompanyRole);
+  deployer.deploy(ManufacturerRole);
+  deployer.deploy(RetailerRole);
+  deployer.deploy(ConsumerRole);
 };
